dd38dffcaab1bfc3149b38e6badccc31a8603566
zjqy30 <969719901@qq.com>
2018-01-15 14:08:56 +0800

    master

--- src/main/java/com/haoyue/HaoyueApplication.java
+++ src/main/java/com/haoyue/HaoyueApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HaoyueApplication {
 
 	public static void main(String[] args) {
-
-		System.out.println("new_master");
+		System.out.println("master");
 		SpringApplication.run(HaoyueApplication.class, args);
 	}
 }
