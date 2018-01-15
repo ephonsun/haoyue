8205277334205536488c2d536832b3a8fb247d33
zjqy30 <969719901@qq.com>
2018-01-15 11:20:47 +0800

    1.master

--- src/main/java/com/haoyue/HaoyueApplication.java
+++ src/main/java/com/haoyue/HaoyueApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HaoyueApplication {
 
 	public static void main(String[] args) {
-
-
 		SpringApplication.run(HaoyueApplication.class, args);
 	}
 }
