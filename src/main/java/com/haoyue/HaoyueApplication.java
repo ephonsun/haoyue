2cead22e46b65dfc783cf15fa8f7aff991b950b9
zjqy30 <969719901@qq.com>
2018-01-15 12:14:00 +0800

    from ye

--- src/main/java/com/haoyue/HaoyueApplication.java
+++ src/main/java/com/haoyue/HaoyueApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HaoyueApplication {
 
 	public static void main(String[] args) {
+		// ye
 		SpringApplication.run(HaoyueApplication.class, args);
 	}
 }
