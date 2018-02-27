29241fa2cf823803408660d37d979c3c38c42119
zjqy30 <969719901@qq.com>
2018-02-27 16:41:56 +0800

    1.点赞领取小程序活动

---
+++ src/main/java/com/haoyue/pojo/ActivityForThumbsUp.java
@@ -0,0 +1,88 @@
+package com.haoyue.pojo;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "activity_thumb_up")
+public class ActivityForThumbsUp {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String openId;
+    private String wxname;
+    private String wxpic;
+    private boolean isowner=false;
+
+    @OneToMany
+    List<ActivityForThumbsUp> list;
+
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:SS", timezone="GMT+8")
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(updatable=false)
+    private Date createDate;//创建日期
+
+
+    public boolean getIsIsowner() {
+        return isowner;
+    }
+
+    public void setIsowner(boolean isowner) {
+        this.isowner = isowner;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+
+    public String getWxname() {
+        return wxname;
+    }
+
+    public void setWxname(String wxname) {
+        this.wxname = wxname;
+    }
+
+    public String getWxpic() {
+        return wxpic;
+    }
+
+    public void setWxpic(String wxpic) {
+        this.wxpic = wxpic;
+    }
+
+    public List<ActivityForThumbsUp> getList() {
+        return list;
+    }
+
+    public void setList(List<ActivityForThumbsUp> list) {
+        this.list = list;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
